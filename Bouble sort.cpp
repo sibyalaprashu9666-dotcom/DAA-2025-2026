e696f675d501028dbd5dd50e2a932997d6f42d43
sibyalaprashu9666-dotcom <sibyalaprashu9666@gmail.com>
2026-07-17 10:17:47 +0530

    Update Bouble sort.cpp

--- Bouble sort.cpp
+++ Bouble sort.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <chrono>
+
 using namespace std;
+using namespace std::chrono;
 
 int main() {
     int n;
@@ -14,6 +17,8 @@ int main() {
         cin >> arr[i];
     }
 
+    auto start = high_resolution_clock::now();
+
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
@@ -24,10 +29,26 @@ int main() {
         }
     }
 
+    auto stop = high_resolution_clock::now();
+
+    auto duration = duration_cast<microseconds>(stop - start);
+
     cout << "\nSorted Array: ";
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
+
+    cout << "\n\nStarting Time : "
+         << duration_cast<microseconds>(start.time_since_epoch()).count()
+         << " microseconds";
+
+    cout << "\nEnding Time   : "
+         << duration_cast<microseconds>(stop.time_since_epoch()).count()
+         << " microseconds";
+
+    cout << "\nExecution Time: "
+         << duration.count()
+         << " microseconds" << endl;
 
     return 0;
 }

0faae0087ca693640708237fe4cda09a775cf75b
sibyalaprashu9666-dotcom <sibyalaprashu9666@gmail.com>
2026-07-17 11:12:14 +0530

    Update Bouble sort.cpp

--- Bouble sort.cpp
+++ Bouble sort.cpp
@@ -4,17 +4,13 @@ using namespace std;
 using namespace std::chrono;
 int main() {
     int a;
-
     cout << "Enter the number of elements: ";
     cin >> a;
-
     int arr[a];
-
     cout << "Enter " << a << " elements: ";
     for (int i = 0; i < a; i++) {
         cin >> arr[i];
     }
-
     auto start = high_resolution_clock::now();
 
     for (int i = 0; i < a - 1; i++) {
@@ -26,27 +22,22 @@ int main() {
             }
         }
     }
-
     auto stop = high_resolution_clock::now();
-
     auto duration = duration_cast<microseconds>(stop - start);
-
     cout << "\nSorted Array: ";
     for (int i = 0; i < a; i++) {
         cout << arr[i] << " ";
     }
+    cout << "\n\nStarting Time : ";
+    cout<< duration_cast<microseconds>(start.time_since_epoch()).count();
+    cout<< " microseconds";
 
-    cout << "\n\nStarting Time : "
-         << duration_cast<microseconds>(start.time_since_epoch()).count()
-         << " microseconds";
+    cout << "\nEnding Time   : ";
+    cout << duration_cast<microseconds>(stop.time_since_epoch()).count();
+    cout << " microseconds";
 
-    cout << "\nEnding Time   : "
-         << duration_cast<microseconds>(stop.time_since_epoch()).count()
-         << " microseconds";
-
-    cout << "\nExecution Time: "
-         << duration.count()
-         << " microseconds" << endl;
-
+    cout << "\nExecution Time: ";
+    cout << duration.count();
+    cout << " microseconds" << endl;
     return 0;
 }

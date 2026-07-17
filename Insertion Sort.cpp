0f87be4f4d23afe3114171a9ad8d8b88d5e57d23
sibyalaprashu9666-dotcom <sibyalaprashu9666@gmail.com>
2026-07-17 13:16:05 +0530

    Update Insertion Sort.cpp

--- Insertion Sort.cpp
+++ Insertion Sort.cpp
@@ -1,51 +1,34 @@
 #include <iostream>
-#include <chrono>
 using namespace std;
-using namespace std::chrono;
-int main() {
-    int a;
-    cout << "Enter the number of elements: ";
-    cin >> a;
-    int arr[a];
-    cout << "Enter " << a << " elements: ";
-    for (int i = 0; i < a; i++) {
-        cin >> arr[i];
-    }
 
-    auto start = high_resolution_clock::now();
-
-    for (int i = 1; i < a; i++) {
-        int key = arr[i];
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];      
         int j = i - 1;
 
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j--;
         }
-
-        arr[j + 1] = key;
+        arr[j + 1] = key;      
     }
+}
 
-    auto stop = high_resolution_clock::now();
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    auto duration = duration_cast<microseconds>(stop - start);
-
-    cout << "\nSorted Array: ";
-    for (int i = 0; i < n; i++) {
+    cout << "Original array: ";
+    for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
-    }
+    cout << endl;
 
-    cout << "\n\nStarting Time : ";
-    cout << duration_cast<microseconds>(start.time_since_epoch()).count();
-    cout << " microseconds";
+    insertionSort(arr, n);
 
-    cout << "\nEnding Time   : ";
-    cout << duration_cast<microseconds>(stop.time_since_epoch()).count();
-    cout << " microseconds";
-
-    cout << "\nExecution Time: ";
-    cout << duration.count();
-    cout << " microseconds" << endl;
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
 
     return 0;
 }
